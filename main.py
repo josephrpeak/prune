c30cf8ea9891e2fc2cdf9f8c0f16369db13cd3a0
Joseph Peak <55969233+josephrpeak@users.noreply.github.com>
2022-04-07 20:35:22 -0500

    added bot message auto-deletion

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import discord
+import re
+import time
+import server_info
+from discord.ext import commands
+from keep_alive import keep_alive
+
 client = commands.Bot(command_prefix = '.')
 
 # initialization
@@ -7,7 +14,7 @@ async def on_ready():
 
 # clear 'amount' number of chat messages (default = 1 including prune command message)
 @client.command()
-async def prune(ctx, amount=2):
+async def prune(ctx, amount=1):
   await ctx.channel.purge(limit=amount+1)
 
 # redirect hyperlinks to links text channel
@@ -16,22 +23,32 @@ async def on_message(message):
   if message.author == client.user:
       return
 
+  # urls for links
   urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',message.content.lower())
-
-  # prevent discord gifs from being filtered
+  # urls for discord gifs, to be excluded from link filtering
   gifs = re.findall('https://tenor.com*', message.content.lower())
 
+  # filter for all links excluding discord gifs
   if(not gifs):
     if urls and message.channel.id == server_info.channel_src:
-      await message.channel.send("<@" + str(message.author.id) + ">" + "I moved your stupid link to the links chat, idiot.")
+      msg = await message.channel.send("<@" + str(message.author.id) + ">" + " I moved your stupid link to the links chat, idiot. (Deleting in 10s)")
       await message.delete()
+
+      # delete bot's message after 10s
+      time.sleep(10)
+      await msg.delete()
+
+      # change bot message destination to links chat
       message.channel.id = server_info.channel_dest
-      
+
+      # ping user who sent link
       await message.channel.send("<@" + str(message.author.id) + ">" + str(message.content))
+
+    # allow for bot commands
     await client.process_commands(message)
   
     message.channel.id = server_info.channel_src
-  
+    
 # ping server to keep bot from logging off
 keep_alive()
 client.run(server_info.TOKEN)
